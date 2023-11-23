efffb44cc3c665851db488ea0ad56603fcaa30ee
badguy-00@github.com <balajisankar555@gmail.com>
2023-11-23 15:09:11 +0530

    Signup & Dashboard

--- app/app.py
+++ app/app.py
@@ -27,30 +27,36 @@ def login():
     if request.method=='POST':
         email=request.form['email']
         password=request.form['password']
-        query="""select password from user_details where username = %s"""
         value=(email,)
+        query="""select password from user_details where username = %s"""
         mycursor.execute(query,value)
-        print(value)
         for row in mycursor.fetchall():
             print(row[0])
             f_password=row[0]
             if(f_password==password):
                 return jsonify({'status':'success'})            
-            else:
-                return jsonify({'status':'fail'})
+        return jsonify({'status':'fail'})
     else:
         return render_template('login.html')
 
 #ROUTER '/SIGN UP'
-@bp.route('/signup',methods=['POST'])
+@bp.route('/signup',methods=['POST','GET'])
 def signup():
-    email=request.form['email']
-    password=request.form['password']
-    query="""insert into user_details (username,password) values (%s,%s)"""
-    value-(email,password)
-    mycursor.execute(query,value)
-    mydb.commit()
-    return render_template('login.html')
+    if(request.method=='POST'):
+        email=request.form['email']
+        password=request.form['password']
+        check="""select * from user_details where username = %s"""
+        value=(email,)
+        mycursor.execute(check,value)
+        if(len(mycursor.fetchall()) > 0):
+            return jsonify({'status':'fail'})
+        query="""insert into user_details (username,password) values (%s,%s)"""
+        value=(email,password)
+        mycursor.execute(query,value)
+        mydb.commit()
+        return jsonify({'status':'success'})  
+    else:
+        return render_template('signup.html')
         
 
 
